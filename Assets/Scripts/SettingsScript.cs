43d2712824fac24549a6346806f15b9918f688ee
Stanley29 <sergii.ky@gmail.com>
2022-09-25 00:47:24 +0300

    settings fixed, audio added, 25092022

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
     public GameObject settingsPanel;
     public GameObject startPanel;
+    public Text CurrentLevelText;
     public static float LevelHardness = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CurrentLevelText.text = "Easy";
     }
 
     // Update is called once per frame
@@ -23,20 +25,25 @@ public class SettingsScript : MonoBehaviour
     {
         LevelHardness = 30;
         settingsPanel.SetActive(false);
-        startPanel.SetActive(true);
+        CurrentLevelText.text = "Easy";
     }
 
     public void MiddleLevelButton()
     {
         LevelHardness = 20;
         settingsPanel.SetActive(false);
-        startPanel.SetActive(true);
+        CurrentLevelText.text = "Middle";
     }
 
     public void HardLevelButton()
     {
         LevelHardness = 10;
         settingsPanel.SetActive(false);
-        startPanel.SetActive(true);
+        CurrentLevelText.text = "Hard";
+    }
+
+    public void SettingsButton()
+    {
+        settingsPanel.SetActive(true);
     }
 }
