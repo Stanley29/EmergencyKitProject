049857d45e58ac7f5193f442d9a8af558ad7ca84
Stanley29 <sergii.ky@gmail.com>
2022-10-01 10:42:43 +0300

    ukrainian language is implemented, 01102022

--- Assets/Scripts/FinalSceneButtonsScript.cs
+++ Assets/Scripts/FinalSceneButtonsScript.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FinalSceneButtonsScript : MonoBehaviour
 {
+    public Text gameCompletedTextEng;
+    public Text restartButtonTextEng;
+    public Text quiteGametButtonTextEng;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (SettingsScript.GameLanguage == "English")
+        {
+            gameCompletedTextEng.text = PreferencesScript.GameCompletedTextEng;
+            restartButtonTextEng.text = PreferencesScript.RestartButtonTextEng;
+            quiteGametButtonTextEng.text = PreferencesScript.QuiteGametButtonTextEng;
+        }
+        else
+        {
+            gameCompletedTextEng.text = PreferencesScript.GameCompletedTextUkr;
+            restartButtonTextEng.text = PreferencesScript.RestartButtonTextUkr;
+            quiteGametButtonTextEng.text = PreferencesScript.QuiteGametButtonTextUkr;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -7,12 +7,35 @@ public class SettingsScript : MonoBehaviour
 {
     public GameObject settingsPanel;
     public GameObject startPanel;
-    public Text CurrentLevelText;
     public static float LevelHardness = 30;
+    public static string GameLanguage = "English";
+
+    //hardness level texts
+    public Text currentLevelText;
+    public Text easyGameLevelButtonText;
+    public Text middleGameLevelButtonText;
+    public Text hardGameLevelButtonText;
+    public Text chooseYourHardnessLevel;
+
+    //main menu texts
+    public Text startButtonText;
+    public Text quitButtonText;
+    public Text settingsButtonText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        CurrentLevelText.text = "Easy";
+        currentLevelText.text = PreferencesScript.EasyGameLevelEng;
+
+        chooseYourHardnessLevel.text = PreferencesScript.ChooseYourHardnessLevelEng;
+        easyGameLevelButtonText.text = PreferencesScript.EasyGameLevelEng;
+        middleGameLevelButtonText.text = PreferencesScript.MiddleGameLevelEng;
+        hardGameLevelButtonText.text = PreferencesScript.HardGameLevelEng;
+
+        startButtonText.text = PreferencesScript.StartButtonTextEng;
+        quitButtonText.text = PreferencesScript.QuitButtonTextEng;
+        settingsButtonText.text = PreferencesScript.SettingsButtonTextEng;
     }
 
     // Update is called once per frame
@@ -25,25 +48,107 @@ public class SettingsScript : MonoBehaviour
     {
         LevelHardness = 30;
         settingsPanel.SetActive(false);
-        CurrentLevelText.text = "Easy";
+
+        if (GameLanguage == "English")
+        {
+            currentLevelText.text = PreferencesScript.EasyGameLevelEng;
+        }
+        else
+        {
+            currentLevelText.text = PreferencesScript.EasyGameLevelUkr;
+        }
     }
 
     public void MiddleLevelButton()
     {
         LevelHardness = 20;
         settingsPanel.SetActive(false);
-        CurrentLevelText.text = "Middle";
+
+        if (GameLanguage == "English")
+        {
+            currentLevelText.text = PreferencesScript.MiddleGameLevelEng;
+        }
+        else
+        {
+            currentLevelText.text = PreferencesScript.MiddleGameLevelUkr;
+        }
     }
 
     public void HardLevelButton()
     {
         LevelHardness = 10;
         settingsPanel.SetActive(false);
-        CurrentLevelText.text = "Hard";
+
+        if (GameLanguage == "English")
+        {
+            currentLevelText.text = PreferencesScript.HardGameLevelEng;
+        }
+        else
+        {
+            currentLevelText.text = PreferencesScript.HardGameLevelUkr;
+        }
     }
 
     public void SettingsButton()
     {
         settingsPanel.SetActive(true);
+    }
+
+    public void SetEnglish()
+    {
+        GameLanguage = "English";
+
+        switch (LevelHardness)
+        {
+            case 10:
+                currentLevelText.text = PreferencesScript.HardGameLevelEng;
+                break;
+            case 20:
+                currentLevelText.text = PreferencesScript.MiddleGameLevelEng;
+                break;
+            case 30:
+                currentLevelText.text = PreferencesScript.EasyGameLevelEng;
+                break;
+            default:
+                break;
+        }
+
+        chooseYourHardnessLevel.text = PreferencesScript.ChooseYourHardnessLevelEng;
+        easyGameLevelButtonText.text = PreferencesScript.EasyGameLevelEng;
+        middleGameLevelButtonText.text = PreferencesScript.MiddleGameLevelEng;
+        hardGameLevelButtonText.text = PreferencesScript.HardGameLevelEng;
+
+        startButtonText.text = PreferencesScript.StartButtonTextEng;
+        quitButtonText.text = PreferencesScript.QuitButtonTextEng;
+        settingsButtonText.text = PreferencesScript.SettingsButtonTextEng;
+    }
+
+    public void SetUkrainian()
+    {
+        GameLanguage = "Ukrainian";
+
+        switch (LevelHardness)
+        {
+            case 10:
+                currentLevelText.text = PreferencesScript.HardGameLevelUkr;
+                break;
+            case 20:
+                currentLevelText.text = PreferencesScript.MiddleGameLevelUkr;
+                break;
+            case 30:
+                currentLevelText.text = PreferencesScript.EasyGameLevelUkr;
+                break;
+            default:
+                break;
+        }
+
+        chooseYourHardnessLevel.text = PreferencesScript.ChooseYourHardnessLevelUkr;
+        easyGameLevelButtonText.text = PreferencesScript.EasyGameLevelUkr;
+        middleGameLevelButtonText.text = PreferencesScript.MiddleGameLevelUkr;
+        hardGameLevelButtonText.text = PreferencesScript.HardGameLevelUkr;
+
+        startButtonText.text = PreferencesScript.StartButtonTextUkr;
+        quitButtonText.text = PreferencesScript.QuitButtonTextUkr;
+        settingsButtonText.text = PreferencesScript.SettingsButtonTextUkr;
     }
 }
